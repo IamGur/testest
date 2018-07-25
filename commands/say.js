ceab9d6a7f62a4cbf1e255ad9b6a8887e42fc3df
IamGur <36259923+IamGur@users.noreply.github.com>
2018-07-25 09:36:22 +0530

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,4 +1,11 @@
+const Dav = '324432889561219072';
+
 exports.run = (client, message, args) => {
+
+    if (message.author.id !== Dav) {
+        message.reply('This Command Is Only For Bot Developer!');
+        return;
+    }
     let say = args.join(' ');
     message.delete();
     message.channel.send(say);

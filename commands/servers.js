1750ed524627951405e530ab0bd80d9c8d9eb827
IamGur <36259923+IamGur@users.noreply.github.com>
2018-07-25 18:36:23 +0530

    Update servers.js

--- commands/servers.js
+++ commands/servers.js
@@ -2,6 +2,10 @@ const Discord = require('discord.js');
 
 exports.run = async (client, message, args) => {
 
-    let guilds = client.guilds.map((guild) => `**${guild.name}** members: ${guild.members.size} id: (${guild.id})`);
+    if (message.author.id !== Dav) {
+        message.reply('This Command Is Only For Bot Developer!');
+        return;
+    }
+    let guilds = client.guilds.map((guild) => `**(${guild.name})**   (**Members:** ${guild.members.size})  (**Id:** ${guild.id})  (**Server Owner:** ${guild.owner.user.tag})`);
     message.channel.send(`I'm on **${client.guilds.size} Servers**:\n${guilds.join ('\n')}`, { split: "\n" })
 }

b0ca2bb8fa5b4e082c3d2c22c322a5f68319c5c8
IamGur <36259923+IamGur@users.noreply.github.com>
2018-08-04 10:23:57 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,17 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js')
 
-exports.run = (client, message, args) => {
-    let pingembed = new Discord.RichEmbed()
-    .setColor('RANDOM')
-    .addField("**Pong🎉🎉! Websocket Latency**:", `${Math.round(client.ping)}ms`)
-    .setTimestamp();
-    message.channel.send({ embed: pingembed })
+exports.run = async (client, message, args, color) => {
+
+    let start = Date.now(); message.channel.send('Pong! ').then(message => { 
+    let diff = (Date.now() - start); 
+    let API = (client.ping).toFixed(2)
+        
+        let embed = new Discord.RichEmbed()
+        .setTitle(`🎉 Pong!`)
+        .setColor('RANDOM')
+        .addField("📶 Latency", `${diff}ms`, true)
+        .addField("💻 API", `${API}ms`, true)
+        .setTimestamp();
+        message.edit(embed);
+    });
 }
